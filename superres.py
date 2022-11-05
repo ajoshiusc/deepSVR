cf16d39eefaf5cb9dbc853b9a679d0d528809485
Anand A. Joshi <ajoshi@usc.edu>
2022-11-05 00:21:09 -0700

    update

--- superres.py
+++ superres.py
@@ -1,11 +1,11 @@
 from monai.utils import set_determinism, first
 from monai.transforms import (
     EnsureChannelFirstD,
-    Compose,ConcatItemsd,
+    Compose, ConcatItemsd,
     LoadImageD,
     RandRotateD,
     RandZoomD,
-    GaussianSmoothd,RandGaussianSmoothd,
+    GaussianSmoothd, RandGaussianSmoothd,
     ScaleIntensityRangePercentilesd, Resized
 )
 from monai.data import DataLoader, Dataset, CacheDataset
@@ -23,18 +23,16 @@ from glob import glob
 from monai.data.nifti_writer import write_nifti
 
 
-
-
 print_config()
 set_determinism(42)
 
 image_files = glob('./feta_syn_data/sub-*_T2w/sub-*_T2w_image.nii.gz')
 
 
-training_datadict = [{"image": item, "stack0": item[:-13]+'_stack_x_0.nii.gz', 
-    "stack1": item[:-13]+'_stack_x_1.nii.gz',"stack2": item[:-13]+'_stack_y_0.nii.gz',
-    "stack3": item[:-13]+'_stack_y_1.nii.gz',"stack4": item[:-13]+'_stack_z_0.nii.gz',
-    "stack5": item[:-13]+'_stack_z_1.nii.gz'} for item in image_files]
+training_datadict = [{"image": item, "stack0": item[:-13]+'_stack_x_0.nii.gz',
+                      "stack1": item[:-13]+'_stack_x_1.nii.gz', "stack2": item[:-13]+'_stack_y_0.nii.gz',
+                      "stack3": item[:-13]+'_stack_y_1.nii.gz', "stack4": item[:-13]+'_stack_z_0.nii.gz',
+                      "stack5": item[:-13]+'_stack_z_1.nii.gz'} for item in image_files]
 
 
 #training_datadict = d0 + d1 + d2 + d3 + d4 + d5
@@ -44,10 +42,14 @@ training_datadict = [{"image": item, "stack0": item[:-13]+'_stack_x_0.nii.gz',
 
 train_transforms = Compose(
     [
-        LoadImageD(keys=["image", "stack0", "stack1", "stack2","stack3","stack4","stack5"]),
-        EnsureChannelFirstD(keys=["image", "stack0", "stack1", "stack2", "stack3", "stack4", "stack5"]),
-        Resized(keys=["stack0", "stack1", "stack2","stack3","stack4","stack5"],spatial_size=[64,64,64]),
-        ConcatItemsd(keys=["stack0", "stack1", "stack2","stack3","stack4","stack5"],name='stacks'),
+        LoadImageD(keys=["image", "stack0", "stack1",
+                   "stack2", "stack3", "stack4", "stack5"]),
+        EnsureChannelFirstD(
+            keys=["image", "stack0", "stack1", "stack2", "stack3", "stack4", "stack5"]),
+        Resized(keys=["stack0", "stack1", "stack2", "stack3",
+                "stack4", "stack5"], spatial_size=[64, 64, 64]),
+        ConcatItemsd(keys=["stack0", "stack1", "stack2",
+                     "stack3", "stack4", "stack5"], name='stacks'),
         #Resized(keys=["image", "stack0", "stack1", "stack2","stack3","stack4","stack5"],spatial_size=[32,32,32]),
         #ScaleIntensityRangePercentilesd(keys=["image", "stack"],lower=0,upper=100,b_min=0.0, b_max=1.0, clip=True),
 
@@ -66,10 +68,11 @@ print(f"stack shape: {stack.shape}")
 plt.figure("check", (12, 6))
 plt.subplot(1, 2, 1)
 plt.title("image")
-plt.imshow(image[:,:,32], cmap="gray")
+plt.imshow(image[:, :, 32], cmap="gray")
 plt.subplot(1, 2, 2)
 plt.title("stack")
-plt.imshow(stack[:,:,32], cmap="gray")
+plt.imshow(stack[:, :, 32], cmap="gray")
+plt.savefig('sample_data.png')
 
 plt.show()
 
@@ -107,25 +110,26 @@ for epoch in range(max_epochs):
         image = batch_data["image"].to(device)
         stacks = batch_data["stacks"].to(device)
 
-
         out_image = model(stacks)
 
-        loss = image_loss(image,out_image)
+        loss = image_loss(image, out_image)
         loss.backward()
         optimizer.step()
         epoch_loss += loss.item()
         #print(f"{step}/{len(train_ds) // train_loader.batch_size}, "f"train_loss: {loss.item():.4f}")
 
-
     epoch_loss /= step
     epoch_loss_values.append(epoch_loss)
 
-    if np.mod(epoch,10) == 0:
-        torch.save(model.state_dict(), './model_64_unet/epoch_'+str(epoch)+'.pth')
-
+    if np.mod(epoch, 10) == 0:
+        torch.save(model.state_dict(),
+                   './model_64_unet/epoch_'+str(epoch)+'.pth')
 
     print(f"epoch {epoch + 1} average loss: {epoch_loss:.4f}")
-    
+
 plt.plot(epoch_loss_values)
+plt.savefig('epochs.png')
+
 plt.show()
+
 print("done")
