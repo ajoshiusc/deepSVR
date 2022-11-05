47b3a6b8fb2cf1e32eb69fcb99a8c2ef99099560
Anand A. Joshi <ajoshi@usc.edu>
2022-11-05 00:17:16 -0700

    update

--- superres.py
+++ superres.py
@@ -1,7 +1,7 @@
 from monai.utils import set_determinism, first
 from monai.transforms import (
     EnsureChannelFirstD,
-    Compose,
+    Compose,ConcatItemsd,
     LoadImageD,
     RandRotateD,
     RandZoomD,
@@ -45,7 +45,9 @@ training_datadict = [{"image": item, "stack0": item[:-13]+'_stack_x_0.nii.gz',
 train_transforms = Compose(
     [
         LoadImageD(keys=["image", "stack0", "stack1", "stack2","stack3","stack4","stack5"]),
-        EnsureChannelFirstD(keys=["image", "stack0", "stack1", "stack2","stack3","stack4","stack5"]),
+        EnsureChannelFirstD(keys=["image", "stack0", "stack1", "stack2", "stack3", "stack4", "stack5"]),
+        Resized(keys=["stack0", "stack1", "stack2","stack3","stack4","stack5"],spatial_size=[64,64,64]),
+        ConcatItemsd(keys=["stack0", "stack1", "stack2","stack3","stack4","stack5"],name='stacks'),
         #Resized(keys=["image", "stack0", "stack1", "stack2","stack3","stack4","stack5"],spatial_size=[32,32,32]),
         #ScaleIntensityRangePercentilesd(keys=["image", "stack"],lower=0,upper=100,b_min=0.0, b_max=1.0, clip=True),
 
@@ -56,18 +58,18 @@ check_ds = Dataset(data=training_datadict, transform=train_transforms)
 check_loader = DataLoader(check_ds, batch_size=1, shuffle=True)
 check_data = first(check_loader)
 image = check_data["image"][0][0]
-stack0 = check_data["stack0"][0][0]
+stack = check_data["stacks"][0][4]
 
 print(f"image shape: {image.shape}")
-print(f"stack0 shape: {stack0.shape}")
+print(f"stack shape: {stack.shape}")
 
 plt.figure("check", (12, 6))
 plt.subplot(1, 2, 1)
 plt.title("image")
 plt.imshow(image[:,:,32], cmap="gray")
 plt.subplot(1, 2, 2)
-plt.title("stack0")
-plt.imshow(stack0[:,:,32], cmap="gray")
+plt.title("stack")
+plt.imshow(stack[:,:,32], cmap="gray")
 
 plt.show()
 
@@ -76,21 +78,20 @@ train_ds = CacheDataset(data=training_datadict, transform=train_transforms,
 train_loader = DataLoader(train_ds, batch_size=16, shuffle=True, num_workers=2)
 
 
-device = torch.device("cuda:0")
-model = GlobalNetRigid(
-    image_size=(32, 32, 32),
-    spatial_dims=3,
-    in_channels=2,  # moving and fixed
-    num_channel_initial=16,
-    depth=4).to(device)
-image_loss = masked_mse #(image,slicevol)MSELoss()
-if USE_COMPILED:
-    warp_layer = Warp(3, "border").to(device)
-else:
-    warp_layer = Warp("bilinear", "border").to(device)
-optimizer = torch.optim.Adam(model.parameters(), 1e-5)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-max_epochs = 500
+model = unet.UNet(
+    spatial_dims=3,
+    in_channels=6,  # moving and fixed
+    out_channels=1,
+    channels=(16, 32, 64, 128, 256),
+    strides=(2, 2, 2, 2),
+    num_res_units=2).to(device)
+image_loss = MSELoss()
+
+optimizer = torch.optim.Adam(model.parameters(), 1e-3)
+
+max_epochs = 5000
 epoch_loss_values = []
 
 
@@ -100,41 +101,28 @@ for epoch in range(max_epochs):
     model.train()
     epoch_loss, step = 0, 0
     for batch_data in train_loader:
-        for sliceno in range(int(32/2)):
-            step += 1
-            optimizer.zero_grad()
+        step += 1
+        optimizer.zero_grad()
 
-            moving = batch_data["image"].to(device)
-            batch_size = moving.shape[0]
+        image = batch_data["image"].to(device)
+        stacks = batch_data["stacks"].to(device)
 
-            fixed = torch.zeros(batch_size,1,32,32,32)
-            slice_ind = torch.tensor(range(2*(sliceno),2*sliceno+1))
- 
-            for s in range(batch_size):
-                dir = batch_data['dir'][s]
-                if dir == 0:
-                    fixed[s,:,slice_ind,:,:] = batch_data['stack'][s,:,slice_ind,:,:]
-                elif dir == 1:
-                    fixed[s,:,:,slice_ind,:] = batch_data['stack'][s,:,:,slice_ind,:]
-                elif dir == 2:
-                    fixed[s,:,:,:,slice_ind] = batch_data['stack'][s,:,:,:,slice_ind]
 
-            fixed = fixed.to(device)
+        out_image = model(stacks)
 
-            ddf = model(torch.cat((moving, fixed), dim=1))
-            pred_image = warp_layer(moving, ddf)
-
-            loss = image_loss(pred_image,fixed)
-            loss.backward()
-            optimizer.step()
-            epoch_loss += loss.item()
+        loss = image_loss(image,out_image)
+        loss.backward()
+        optimizer.step()
+        epoch_loss += loss.item()
         #print(f"{step}/{len(train_ds) // train_loader.batch_size}, "f"train_loss: {loss.item():.4f}")
 
 
     epoch_loss /= step
     epoch_loss_values.append(epoch_loss)
 
-    torch.save(model.state_dict(), './model_32_slice2vol/epoch_'+str(epoch)+'.pth')
+    if np.mod(epoch,10) == 0:
+        torch.save(model.state_dict(), './model_64_unet/epoch_'+str(epoch)+'.pth')
+
 
     print(f"epoch {epoch + 1} average loss: {epoch_loss:.4f}")
     
