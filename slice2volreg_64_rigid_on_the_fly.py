b626e92a922a2cc47f505684948ab1bba9d3c3eb
Anand A. Joshi <ajoshi@usc.edu>
2022-11-20 23:46:11 -0800

    update

--- slice2volreg_64_rigid_on_the_fly.py
+++ slice2volreg_64_rigid_on_the_fly.py
@@ -1,11 +1,11 @@
 from monai.utils import set_determinism, first
 from monai.transforms import (
     EnsureChannelFirstD,
-    Compose,ConcatItemsd,
-    LoadImageD,CopyItemsd,
-    RandRotateD,RandAffined,
-    RandZoomD,CropForegroundd,
-    GaussianSmoothd,RandGaussianSmoothd,
+    Compose, ConcatItemsd,
+    LoadImageD, CopyItemsd,
+    RandRotateD, RandAffined,
+    RandZoomD, CropForegroundd,
+    GaussianSmoothd, RandGaussianSmoothd,
     ScaleIntensityRangePercentilesd, Resized
 )
 from monai.data import DataLoader, Dataset, CacheDataset
@@ -23,12 +23,12 @@ import tempfile
 from glob import glob
 from monai.data.nifti_writer import write_nifti
 
-def masked_mse(moving,slicevol):
-    msk = slicevol>0
+
+def masked_mse(moving, slicevol):
+    msk = slicevol > 0
     err = (moving-slicevol)*msk
     loss = torch.sqrt(torch.sum(err**2))
     return loss
-
 
 
 print_config()
@@ -62,21 +62,23 @@ randstack_transforms = Compose(
         RandAffined(mode=("bilinear"), prob=1.0, translate_range=(5, 5, 5), rotate_range=(
             np.pi/4, np.pi/4, np.pi/4), padding_mode="zeros", keys=["image"]),
 
-        CopyItemsd(keys=["image", "image", "image", "image", "image", "image"], names=[
-                   "stack0", "stack1", "stack2", "stack3", "stack4", "stack5"]),
-        RandAffined(mode=("bilinear"), prob=1.0, translate_range=(.2, 1, 1), rotate_range=(
+        CopyItemsd(keys=["image"], names=["stack"]),
+        RandMakeStackd(mode=("bilinear"), prob=1.0, translate_range=(5, 5, 5), rotate_range=(
+            np.pi/4, np.pi/4, np.pi/4), padding_mode="zeros", axis=0, keys=["stack0"]),
+ 
+        RandMakeStackd(mode=("bilinear"), prob=1.0, translate_range=(.2, 1, 1), rotate_range=(
             np.pi / 16, np.pi / 32, np.pi / 32), padding_mode="border", keys=["stack0"]),
-        RandAffined(mode=("bilinear"), prob=1.0, translate_range=(.2, 1, 1), rotate_range=(
+        RandMakeStackd(mode=("bilinear"), prob=1.0, translate_range=(.2, 1, 1), rotate_range=(
             np.pi / 16, np.pi / 32, np.pi / 32), padding_mode="border", keys=["stack1"]),
 
-        RandAffined(mode=("bilinear"), prob=1.0, translate_range=(1, .2, 1), rotate_range=(
+        RandMakeStackd(mode=("bilinear"), prob=1.0, translate_range=(1, .2, 1), rotate_range=(
             np.pi / 32, np.pi / 16, np.pi / 32), padding_mode="border", keys=["stack2"]),
-        RandAffined(mode=("bilinear"), prob=1.0, translate_range=(1, .2, 1), rotate_range=(
+        RandMakeStackd(mode=("bilinear"), prob=1.0, translate_range=(1, .2, 1), rotate_range=(
             np.pi / 32, np.pi / 16, np.pi / 32), padding_mode="border", keys=["stack3"]),
 
-        RandAffined(mode=("bilinear"), prob=1.0, translate_range=(1, 1, .2), rotate_range=(
+        RandMakeStackd(mode=("bilinear"), prob=1.0, translate_range=(1, 1, .2), rotate_range=(
             np.pi / 32, np.pi / 32, np.pi / 16), padding_mode="border", keys=["stack0"]),
-        RandAffined(mode=("bilinear"), prob=1.0, translate_range=(1, 1, .2), rotate_range=(
+        RandMakeStackd(mode=("bilinear"), prob=1.0, translate_range=(1, 1, .2), rotate_range=(
             np.pi / 32, np.pi / 32, np.pi / 16), padding_mode="border", keys=["stack1"]),
 
         Resized(keys=["stack0"], spatial_size=[16, 64, 64]), Resized(
@@ -134,7 +136,7 @@ model = GlobalNetRigid(
     in_channels=2,  # moving and fixed
     num_channel_initial=16,
     depth=4).to(device)
-image_loss = masked_mse #(image,slicevol)MSELoss()
+image_loss = masked_mse  # (image,slicevol)MSELoss()
 if USE_COMPILED:
     warp_layer = Warp(3, "border").to(device)
 else:
@@ -159,35 +161,38 @@ for epoch in range(max_epochs):
             moving = batch_data["image"].to(device)
             batch_size = moving.shape[0]
 
-            fixed = torch.zeros(batch_size,1,64,64,64)
-            slice_ind = torch.tensor(range(4*(sliceno),4*sliceno+1))
- 
+            fixed = torch.zeros(batch_size, 1, 64, 64, 64)
+            slice_ind = torch.tensor(range(4*(sliceno), 4*sliceno+1))
+
             for s in range(batch_size):
                 dir = batch_data['dir'][s]
                 if dir == 0:
-                    fixed[s,:,slice_ind,:,:] = batch_data['stack'][s,:,slice_ind,:,:]
+                    fixed[s, :, slice_ind, :,
+                          :] = batch_data['stack'][s, :, slice_ind, :, :]
                 elif dir == 1:
-                    fixed[s,:,:,slice_ind,:] = batch_data['stack'][s,:,:,slice_ind,:]
+                    fixed[s, :, :, slice_ind,
+                          :] = batch_data['stack'][s, :, :, slice_ind, :]
                 elif dir == 2:
-                    fixed[s,:,:,:,slice_ind] = batch_data['stack'][s,:,:,:,slice_ind]
+                    fixed[s, :, :, :, slice_ind] = batch_data['stack'][s,
+                                                                       :, :, :, slice_ind]
 
             fixed = fixed.to(device)
 
             ddf = model(torch.cat((moving, fixed), dim=1))
             pred_image = warp_layer(moving, ddf)
 
-            loss = image_loss(pred_image,fixed)
+            loss = image_loss(pred_image, fixed)
             loss.backward()
             optimizer.step()
             epoch_loss += loss.item()
         #print(f"{step}/{len(train_ds) // train_loader.batch_size}, "f"train_loss: {loss.item():.4f}")
 
-
     epoch_loss /= step
     epoch_loss_values.append(epoch_loss)
 
     if np.mod(epoch, 10) == 0:
-        torch.save(model.state_dict(), './model_64_slice2vol/epoch_'+str(epoch)+'.pth')
+        torch.save(model.state_dict(),
+                   './model_64_slice2vol/epoch_'+str(epoch)+'.pth')
         valid_loss = 0
         for valid_batch_data in valid_loader:
             valid_image = valid_batch_data["image"].to(device)
@@ -198,64 +203,10 @@ for epoch in range(max_epochs):
 
         print(f"validation loss: {valid_loss:.4f}")
 
-
     print(f"epoch {epoch + 1} average loss: {epoch_loss:.4f}")
 
-  
-#plt.plot(epoch_loss_values)
+np.savez('svr_reg_epoch_loss_values.npz',
+         epoch_loss_values=epoch_loss_values, epoch_loss_valid=epoch_loss_valid)
 
-batch_size=16
-val_ds = CacheDataset(data=training_datadict[0:16], transform=train_transforms,
-                      cache_rate=1.0, num_workers=0)
-val_loader = DataLoader(val_ds, batch_size=16, num_workers=0)
-for batch_data in val_loader:
-    moving = batch_data["image"].to(device)
-    fixed_full = batch_data["stack"]
-    dir = batch_data['dir']
 
-    fixed = torch.zeros(batch_size,1,64,64,64)
-    fixed[:,0,16:18,:,:]=fixed_full[:,0,16:18,:,:]
-    fixed = fixed.to(device)
-    ddf = model(torch.cat((moving, fixed), dim=1))
-    pred_image = warp_layer(moving, ddf)
-    break
-
-fixed_image = fixed.detach().cpu().numpy()[:, 0]
-moving_image = moving.detach().cpu().numpy()[:, 0]
-pred_image = pred_image.detach().cpu().numpy()[:, 0]
-
-for i in range(batch_size):
-    write_nifti(fixed_image[i],'fixed'+str(i)+'.nii.gz')
-    write_nifti(moving_image[i],'moving'+str(i)+'.nii.gz')
-    write_nifti(pred_image[i],'pred'+str(i)+'.nii.gz')
-
-#batch_size = 5
-'''plt.subplots(batch_size, 3, figsize=(8, 10))
-for b in range(batch_size):
-    # moving image
-    plt.subplot(batch_size, 3, b * 3 + 1)
-    plt.axis('off')
-    plt.title("moving image")
-    plt.imshow(moving_image[b][:,:,16], cmap="gray")
-    # fixed image
-    plt.subplot(batch_size, 3, b * 3 + 2)
-    plt.axis('off')
-    plt.title("fixed image")
-    plt.imshow(fixed_image[b][:,:,16], cmap="gray")
-    # warped moving
-    plt.subplot(batch_size, 3, b * 3 + 3)
-    plt.axis('off')
-    plt.title("predicted image")
-    plt.imshow(pred_image[b][:,:,16], cmap="gray")
-plt.axis('off')
-plt.show()
-
-'''
 print('Done!')
-
-
-
-
-
-
-
