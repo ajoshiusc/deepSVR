9e4710e30aad813f0943619a90dc08176b8dd6b6
Anand A. Joshi <ajoshi@usc.edu>
2022-11-29 09:49:53 -0800

    update

--- eval_superres_large_unet_on_the_fly_easy.py
+++ eval_superres_large_unet_on_the_fly_easy.py
@@ -27,7 +27,7 @@ from monai.data.nifti_writer import write_nifti
 from easy_transforms import RandMakeStackd
 
 
-MODEL_FILE = '/project/ajoshi_27/code_farm/deepSVR/model_64_unet_large_lrem4_hcp_easy/epoch_1060.pth'
+MODEL_FILE = '/project/ajoshi_27/code_farm/deepSVR/model_64_unet_large_lrem4_hcp_easy/epoch_10.pth'
 #MODEL_FILE = '/home/ajoshi/epoch_370.pth'
 
 print_config()
